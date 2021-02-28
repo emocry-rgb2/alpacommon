fb0436ea467c51dd0a28b6c4f2f0fac3d02636d7
emocry-rgb2 <kreept.sama@gmail.com>
2021-02-28 15:18:50 +0300

    add changes in index.js

---
+++ www/.cache/sync-requires.js
@@ -0,0 +1,14 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/emocry/myrepo/alpacommon/www/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/emocry/myrepo/alpacommon/www/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/home/emocry/myrepo/alpacommon/www/src/pages/about.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/emocry/myrepo/alpacommon/www/src/pages/index.js"))),
+  "component---src-templates-portfolio-item-jsx": hot(preferDefault(require("/home/emocry/myrepo/alpacommon/www/src/templates/portfolio-item.jsx")))
+}
+
